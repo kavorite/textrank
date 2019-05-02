4031ab5d95456b5a5f89e91485d9e2833f50e476
kavorite <iseurie@gmail.com>
2019-05-01 21:31:01 -0400

    stem test

--- lib_test.go
+++ lib_test.go
@@ -6,17 +6,33 @@ import (
 	"strings"
 )
 
-func TestInspec(t *testing.T) {
+var inspec string
+
+func init() {
 	src, err := ioutil.ReadFile("inspec.txt")
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
+	inspec = string(src)
+}
+
+func TestInspec(t *testing.T) {
 	S := DefaultStops
-	K := TextRank(string(src), 2, S)
-	for w := range K.RankMap() {
+	T := Tokenize(inspec, DefaultStops)
+	K := TextRank(T, 2)
+	R := K.RankMap()
+	for w := range R {
 		if S.Contains(w) || strings.TrimSpace(w) == "" {
 			t.Fatalf("Stop-word found in TextRank payload: %s", w)
 		}
 	}
 	t.Log(K.Tokens)
+}
+
+func TestStem(t *testing.T) {
+	T := Tokenize(inspec, DefaultStops)
+	err := T.Stem("english")
+	if err != nil {
+		t.Fatal(err)
+	}
 }
